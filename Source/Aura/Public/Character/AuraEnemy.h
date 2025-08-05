966b581b119639d3d892b2b9ec97bbbd2a10d10e
dugulanhun <dugulanhun@163.com>
2025-08-06 01:30:20 +0800

    Commit -m "Post Process Highlight"

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -15,9 +15,9 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
 public:
+	AAuraEnemy();
+	
 	virtual void HightlightActor() override;
 	virtual void UnHightlightActor() override;
-
-	UPROPERTY(BlueprintReadOnly)
-	bool bHightlighted = false;	
+	
 };
