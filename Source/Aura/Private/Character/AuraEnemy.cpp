1ca689b6b856d23c25eeee8639b5d7ad97f997ab
dugulanhun <dugulanhun@163.com>
2025-08-07 15:32:13 +0800

    Init Ability Actor Info

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -31,3 +31,10 @@ void AAuraEnemy::UnHightlightActor()
 	GetMesh()->SetRenderCustomDepth(false);
 	Weapon->SetRenderCustomDepth(false);
 }
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AbilitySystemComponent->InitAbilityActorInfo(this, this);
+}
--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -16,8 +16,11 @@ class AURA_API AAuraEnemy : public AAuraCharacterBase, public IEnemyInterface
 	GENERATED_BODY()
 public:
 	AAuraEnemy();
-	
+
+	/** Enemy Interface */
 	virtual void HightlightActor() override;
 	virtual void UnHightlightActor() override;
-	
+	/** End Enemy Interface */
+protected:
+	virtual void BeginPlay() override;
 };
