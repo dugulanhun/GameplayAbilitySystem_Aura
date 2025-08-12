698fe99d4c04616798a0a0fd78832c0e773f7444
dugulanhun <dugulanhun@163.com>
2025-08-12 23:09:34 +0800

    Gameplay Tags Singleton

---
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -0,0 +1,13 @@
+// Copyright cfy
+
+
+#include "AuraGameplayTags.h"
+#include "GameplayTagsManager.h"
+
+FAuraGameplayTags FAuraGameplayTags::GameplayTags;
+
+void FAuraGameplayTags::InitializeNativeGameplayTags()
+{
+	UGameplayTagsManager::Get().AddNativeGameplayTag(FName("Attribute.Secondary.Armor"), FString("Reduce damage taken, improves Block Chance"));
+	
+}
