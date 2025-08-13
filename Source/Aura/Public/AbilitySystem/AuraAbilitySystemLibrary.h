00944dd6029d6fc9c2a0b3e0c44733f2765aa8e1
dugulanhun <dugulanhun@163.com>
2025-08-13 17:38:43 +0800

    Aura Ability System Blueprint Library

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemLibrary.h
@@ -0,0 +1,20 @@
+// Copyright cfy
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "AuraAbilitySystemLibrary.generated.h"
+
+class UOverlayWidgetController;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+public:
+	UFUNCTION(BlueprintPure, Category = "AbilitySystemLibrary|WidgetController")
+	static UOverlayWidgetController* GetOverlayWidgetController(const UObject* WorldContextObject);
+};
